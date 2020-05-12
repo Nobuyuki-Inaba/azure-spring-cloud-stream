043f59db846226f3d34d5d86c932438233f80266
nobuyuki.inaba <nobuyuki178jp@yahoo.co.jp>
2020-05-12 20:02:04 +0900

    add reproduce sources

---
+++ src/main/java/com/example/springcloudeventhub/EventhubSink.java
@@ -0,0 +1,27 @@
+package com.example.springcloudeventhub;
+
+import com.microsoft.azure.spring.integration.core.AzureHeaders;
+import com.microsoft.azure.spring.integration.core.api.Checkpointer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.messaging.handler.annotation.Header;
+
+@EnableBinding(Sink.class)
+public class EventhubSink {
+
+   private static final Logger LOGGER = LoggerFactory.getLogger(EventhubSink.class);
+
+   @StreamListener(Sink.INPUT)
+   public void handleMessage(String message, @Header(AzureHeaders.CHECKPOINTER) Checkpointer checkpointer) {
+      LOGGER.info("New message received: '{}'", message);
+      checkpointer.success().handle((r, ex) -> {
+         if (ex == null) {
+            LOGGER.info("Message '{}' successfully checkpointed", message);
+         }
+         return null;
+      });
+   }
+}
---
+++ src/main/java/com/example/springcloudeventhub/SpringcloudeventhubApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcloudeventhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudeventhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudeventhubApplication.class, args);
+	}
+
+}
